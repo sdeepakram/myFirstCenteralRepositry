c8b3c47f48587262dd6797d31b14836fc6a13932
bgovindamma <bgovindamma58@gmail.com>
2023-07-15 13:39:24 -0700

    done

---
+++ ProtectedTest1.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class ProtectedTest1 {
+
+	public static void main(String[] args) {
+		ProtectDemo1 p1=new ProtectDemo1();
+		System.out.println(p1.a);
+		p1.funA();
+
+	}
+
+}
